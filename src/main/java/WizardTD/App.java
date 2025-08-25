d1660d564b6e8c081684916ec7b3693a82e5fceb
K-Maddox <kmad5568@gmail.com>
2025-08-25 20:58:35 +1000

    Add imports to App.java

--- src/main/java/WizardTD/App.java
+++ src/main/java/WizardTD/App.java
@@ -1,6 +1,11 @@
 package WizardTD;
 
 import processing.core.PApplet;
+import processing.core.PGraphics;
+import processing.core.PImage;
+import processing.data.JSONArray;
+import processing.data.JSONObject;
+import processing.event.MouseEvent;
 
 public class App extends PApplet {
 
