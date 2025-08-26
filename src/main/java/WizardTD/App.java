6d476a80a29ce86374dd4f6095580f770f7ee6ab
K-Maddox <kmad5568@gmail.com>
2025-08-26 20:11:59 +1000

    Add imports to App.java

--- src/main/java/WizardTD/App.java
+++ src/main/java/WizardTD/App.java
@@ -7,6 +7,26 @@ import processing.data.JSONArray;
 import processing.data.JSONObject;
 import processing.event.MouseEvent;
 
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+
+import java.io.*;
+import java.util.*;
+
 public class App extends PApplet {
 
+public static final int CELLSIZE = 32;
+    public static final int SIDEBAR = 120;
+    public static final int TOPBAR = 40;
+    public static final int BOARD_WIDTH = 20;
+
+    public static int WIDTH = CELLSIZE*BOARD_WIDTH+SIDEBAR;
+    public static int HEIGHT = BOARD_WIDTH*CELLSIZE+TOPBAR;
+
+    public static final int FPS = 60;
+
+    public String configPath;
+
+    public Random random = new Random();
 }
