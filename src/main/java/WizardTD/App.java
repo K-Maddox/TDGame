86ac4f8d68726889e5c333eb62b1c08372ea8bee
K-Maddox <kmad5568@gmail.com>
2025-08-28 19:58:18 +1000

    Initialise Images

--- src/main/java/WizardTD/App.java
+++ src/main/java/WizardTD/App.java
@@ -29,4 +29,31 @@ public static final int CELLSIZE = 32;
     public String configPath;
 
     public Random random = new Random();
+
+    public int timer;
+
+    //initialise images
+    public PImage beetleImage;
+    public PImage fireballImage;
+    public PImage grassImage;
+    public PImage gremlinImage;
+    public PImage gremlin1Image;
+    public PImage gremlin2Image;
+    public PImage gremlin3Image;
+    public PImage gremlin4Image;
+    public PImage gremlin5Image;
+    public PImage path0Image;
+    public PImage path0ImageRotate;
+    public PImage path1Image;
+    public PImage path1ImageRotate;
+    public PImage path2Image;
+    public PImage path2ImageRotate;
+    public PImage path3Image;
+    public PImage path3ImageRotate;
+    public PImage shrubImage;
+    public PImage tower0Image;
+    public PImage tower1Image;
+    public PImage tower2Image;
+    public PImage wizardHouseImage;
+    public PImage wormImage;
 }
