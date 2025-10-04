ec75e57acf19ac66669582eb65cc6662c0199748
K-Maddox <kmad5568@gmail.com>
2025-10-04 15:43:14 +1000

    add Button attributes

--- src/main/java/WizardTD/Button.java
+++ src/main/java/WizardTD/Button.java
@@ -5,5 +5,21 @@ package WizardTD;
  */
 public class Button {
     
-    
+    protected int x;
+    protected int y;
+    protected int width;
+    protected int height;
+    protected int textSize;
+    protected int textColour = 0; //default black
+    protected String textMessage;
+    protected int textAlign = 3;
+
+    public boolean clicked;
+    public boolean activeFlag = false;
+    public boolean boxFlag;
+    public boolean hoverFlagTower = false;
+    public boolean hoverFlagMana = false;
+    public boolean waveFlag = false;
+    public boolean manaFlag = false;
+    public boolean manaSpellFlag = false;
 }
