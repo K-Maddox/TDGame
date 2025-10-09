f7f21cebed1a23b46332f8b647793afee29955c0
K-Maddox <kmad5568@gmail.com>
2025-10-09 22:46:51 +1100

    create absract super class for all objects

---
+++ src/main/java/WizardTD/GameObject.java
@@ -0,0 +1,13 @@
+package WizardTD;
+
+import processing.core.PApplet;
+import processing.core.PImage;
+import java.util.*;
+
+/**
+ * Abstract super class for all objects
+ */
+public abstract class GameObject {
+    
+    
+}
