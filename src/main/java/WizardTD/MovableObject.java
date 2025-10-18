fe430f96d2c2cf93cfdfb9fdd45aaaee70a371f5
K-Maddox <kmad5568@gmail.com>
2025-10-18 14:24:28 +1100

    extend Game Objects

---
+++ src/main/java/WizardTD/MovableObject.java
@@ -0,0 +1,12 @@
+package WizardTD;
+
+import processing.core.PApplet;
+import processing.core.PImage;
+
+/**
+ * extension of Game Objects for all objects that move i.e. monsters and fireball
+ */
+public class MovableObject extends GameObject {
+
+    
+}
