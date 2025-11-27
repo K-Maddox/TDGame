5d1974a68742333bed58fea1c54b2e9cffbc7a54
K-Maddox <kmad5568@gmail.com>
2025-11-27 22:26:51 +1100

    add wave methods

--- src/main/java/WizardTD/Waves.java
+++ src/main/java/WizardTD/Waves.java
@@ -36,5 +36,67 @@ public class Waves {
         this.quantity = quantity;
     }
 
-    
+    /**
+     * returns duration of wave
+     * @return duration of wave
+     */
+    public int getDuration(){
+        return this.duration;
+    }
+
+    /**
+     * returns pre wave pause
+     * @return pre wave pause of wave
+     */
+    public float getPreWavePause(){
+        return this.preWavePause;
+    }
+
+    /**
+     * returns monster type used in wave
+     * @return monster name
+     */
+    public String getMonster(){
+        return this.monster;
+    }
+
+    /**
+     * returns monster hp
+     * @return hp
+     */
+    public int getHp(){
+        return this.hp;
+    }
+
+    /**
+     * returns monster speed
+     * @return speed
+     */
+    public float getSpeed(){
+        return this.speed;
+    }
+
+    /**
+     * returns monster armour
+     * @return armour
+     */
+    public float getArmour(){
+        return this.armour;
+    }
+
+    /**
+     * returns mana gained from monster kill
+     * @return mana gained
+     */
+    public int getManaGained(){
+        return this.manaGained;
+    }
+
+    /**
+     * returns the quantity of monsters spawned for the round
+     * @return quantity
+     */
+    public int getQuantity(){
+        return this.quantity;
+    }
 }
