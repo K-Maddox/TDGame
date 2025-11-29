47918f01d577eb10731a906d43f963acd8ecabda
K-Maddox <kmad5568@gmail.com>
2025-11-29 13:09:52 +1100

    add tower class

---
+++ src/main/java/WizardTD/Tower.java
@@ -0,0 +1,12 @@
+package WizardTD;
+
+import processing.core.PImage;
+import java.util.*;
+
+/**
+ * extension of Game Object - shoots fireballs at enemy within its range
+*/
+public class Tower extends GameObject {
+
+    
+}
