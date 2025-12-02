af466edf05c32d4ae55479d468b5411c9f7f7e76
K-Maddox <kmad5568@gmail.com>
2025-12-02 21:32:33 +1100

    add fireball constructor

--- src/main/java/WizardTD/Fireball.java
+++ src/main/java/WizardTD/Fireball.java
@@ -17,5 +17,21 @@ public class Fireball extends MovableObject {
     protected int xVel = this.speed;
     protected int yVel = this.speed;
 
+    /**
+     * Constructor for Fireballs initialises the below attributes
+     * @param x
+     * @param y
+     * @param sprite
+     * @param damage
+     * @param target
+     */
+    public Fireball(int x, int y, PImage sprite, int damage, Monster target){
+        super(x, y, sprite);
+        this.target = target;
+        this.damage = damage;
+        this.destX = target.getX();
+        this.destY = target.getY();
+    }
+
     
 }
