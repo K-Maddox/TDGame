ef0fbda909f664b2b276a964fa5273d29c8190ac
K-Maddox <kmad5568@gmail.com>
2025-12-02 21:31:55 +1100

    add fireball class

---
+++ src/main/java/WizardTD/Fireball.java
@@ -0,0 +1,11 @@
+package WizardTD;
+
+import processing.core.PImage;
+
+/**
+ * Extension of MovableObject - they are shot from towers and can damage or kill monsters
+*/
+public class Fireball extends MovableObject {
+    
+    
+}
