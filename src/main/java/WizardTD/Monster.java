70087e299bc366f839eac5a5ef209b3dc155bc49
K-Maddox <kmad5568@gmail.com>
2025-12-06 13:24:57 +1100

    add monster class

---
+++ src/main/java/WizardTD/Monster.java
@@ -0,0 +1,13 @@
+package WizardTD;
+
+import processing.core.PImage;
+import java.awt.Point;
+import java.util.*;
+
+/**
+ * Extension of MovableObject - this stores all attributes for enemy objects
+ */
+public class Monster extends MovableObject {
+
+    
+}
